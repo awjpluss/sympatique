4487a08d58c819c5b7c391e7962eac2ec5b832bf
awjpluss <mal.102030.111@gmail.com>
2026-08-25 02:31:04 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // سيمباتيك - إعدادات داخلية - لا تظهر للعملاء
 const SYMPATIK_CONFIG = {
-  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQrfbagAo0ZHday6kU0rI-sBCrwdqcpiKzgcbSpynJ9hIVqPz455PrHF3WJhk6t8-uug98hm4nAiaOX/pub?output=csv",
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-Z0atKexHAXNSWEpUq6S6uX0ZcJJ7Ehx9SvOwVZzmbx5HiIRcIFYp6rf9NHgwr1YvuVKuHwMtgqq9/pub?output=csv",
   WHATSAPP_NUMBER: "967734405949",
   FREE_SHIPPING_THRESHOLD: 20000,
   FREE_CITIES: ["صنعاء","عمران"],
